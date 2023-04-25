722235731084b3bd529733b4eb7ad0c714c3d599
MrKearns <jkearns631@students.mchenry.edu>
2023-04-25 10:21:20 -0500

    Choose screem amd added Summary screen for next time

---
+++ app/src/main/java/com/example/mad_105_guided_lab/Summary.kt
@@ -0,0 +1,11 @@
+package com.example.mad_105_guided_lab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Summary : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_summary)
+    }
+}
